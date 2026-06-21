ade4afb43e9cf18a5d2cc6b11f70900b0cd4fc5b
Alex-Power-programmer <pythonalex84@gmail.com>
2026-06-21 20:37:59 +0300

    My first Django-project

---
+++ README.md
@@ -0,0 +1,4 @@
+"Django-project"
+```My first Django-project!!```
+
+
--- ads/admin.py
+++ ads/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from ads.models import Categories, Ads
 
-# Register your models here.
-
 
 admin.site.register(Categories)
 admin.site.register(Ads)
